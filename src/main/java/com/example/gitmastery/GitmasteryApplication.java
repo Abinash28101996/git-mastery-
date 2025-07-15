e13aaabeb3a64fe4e8f43f2939138f21e598cee6
Abinash <abinashpatowary842@gmail.com>
2025-07-15 22:42:35 +0530

    First Commit

---
+++ src/main/java/com/example/gitmastery/GitmasteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmasteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitmasteryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmastery/GitmasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmasteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
